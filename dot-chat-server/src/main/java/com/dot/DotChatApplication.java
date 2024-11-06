9f683833801f257394233edcd3a68d7b93908239
daoyang <lidaoyang@hotmail.com>
2024-11-06 15:12:32 +0800

    refactor(dot-chat-server):调整 MyBatis-Plus 分页插件配置和 Mapper 扫描范围
    - 注释掉 CommBeanConfig 中的 mybatisPlusInterceptor 方法
    - 修改 DotChatApplication 中的 @MapperScan 注解，仅扫描 msg 模块的 DAO

--- dot-chat-server/src/main/java/com/dot/DotChatApplication.java
+++ dot-chat-server/src/main/java/com/dot/DotChatApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.dot.msg", "com.dot.sys", "com.dot.comm.config", "com.dot.comm.manager", "com.dot.comm.exception"})
-@MapperScan(basePackages = {"com.dot.sys.*.dao", "com.dot.msg.*.dao"})
+@MapperScan(basePackages = {"com.dot.msg.*.dao"})
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DotChatApplication {
 
