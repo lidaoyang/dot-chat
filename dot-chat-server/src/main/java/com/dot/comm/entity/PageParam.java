7fa5a8c8a2539a15c25a4cee2d704369b4628f09
daoyang <lidaoyang@hotmail.com>
2024-11-14 16:36:43 +0800

    refactor(dot-chat-server): 重命名分页参数类并添加 Swagger 注解
    - 将 PageParamRequest 类重命名为 PageParam
    - 添加 Swagger 注解以描述分页参数对象
    - 更新类注释以反映新的用途

--- dot-chat-server/src/main/java/com/dot/comm/entity/PageParam.java
+++ dot-chat-server/src/main/java/com/dot/comm/entity/PageParam.java
@@ -1,20 +1,25 @@
 package com.dot.comm.entity;
 
 import com.dot.comm.constants.CommConstant;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 /**
- * 分页公共请求对象
+ * 分页参数对象
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PageParamRequest {
+@Schema(name = "PageParam", description = "分页参数对象")
+public class PageParam {
 
+    @Schema(description = "当前页码", defaultValue = "1")
     private int page = CommConstant.DEFAULT_PAGE;
 
+    @Schema(description = "每页数量", defaultValue = "20")
     private int limit = CommConstant.DEFAULT_LIMIT;
 
 }
