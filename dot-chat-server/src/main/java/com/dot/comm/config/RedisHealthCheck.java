73953524970789ed9fe786af3c9023b4b13199d9
daoyang <lidaoyang@hotmail.com>
2024-12-13 14:17:36 +0800

    feat(dot-chat-server): 添加 Redis健康检查功能
    - 在 application-dev.yml、application-local.yml 和 application-prod.yml 中添加 health-check 配置项
    - 新增 RedisHealthCheck 类，用于定期检查 Redis 连接状态
    - 在生产环境中将 Redis 数据库改为0

---
+++ dot-chat-server/src/main/java/com/dot/comm/config/RedisHealthCheck.java
@@ -0,0 +1,40 @@
+package com.dot.comm.config;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+/**
+ * Redis健康检查
+ *
+ * @author: Dao-yang.
+ * @date: Created in 2024/12/13 14:10
+ */
+@Slf4j
+@EnableScheduling
+@Component
+@ConditionalOnProperty(name = "spring.data.redis.health-check", havingValue = "true")
+public class RedisHealthCheck {
+
+    @Resource
+    private StringRedisTemplate redisTemplate;
+
+    // 每分钟执行一次
+    @Scheduled(fixedRate = 60000)
+    public void healthCheck() {
+        try {
+            // 执行Redis PING命令，检测连接是否活跃
+            String result = Objects.requireNonNull(redisTemplate.getConnectionFactory()).getConnection().ping();
+            log.info("Redis PING Response: {}", result);
+        } catch (Exception e) {
+            // 处理Redis连接异常
+            log.error("Redis heartbeat failed", e);
+        }
+    }
+}
