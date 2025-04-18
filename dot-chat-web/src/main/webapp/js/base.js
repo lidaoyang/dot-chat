53bb72ab657bf17623f3fc5d4bebfc62c2a19d4c
daoyang <lidaoyang@hotmail.com>
2025-04-18 20:08:38 +0800

    refactor(dot-chat-web):调整 API 域名和端口配置
    - 为 dot1.chat 生产环境添加独立的 API 域名
    -统一配置 HTTPS 协议- 调整端口配置位置，使其与 HOST 配置对应

--- dot-chat-web/src/main/webapp/js/base.js
+++ dot-chat-web/src/main/webapp/js/base.js
@@ -12,12 +12,12 @@ if (location.host === "dot-chat.jrmall.cn") {
     HOST = "dot-chat.api.jrmall.cn";
 } else if (location.host === "www.dot1.chat") {
     HOST = "api.dot1.chat";
+    BASE_URL = "https://" + HOST + "/";
 } else {
     HOST = "dev.dot.cn";
     ws_port = "9326";
+    BASE_URL = "https://" + HOST + ":8089/";
 }
-BASE_URL = "https://" + HOST + ":8089/";
-
 // 定义常量对象
 const SYS_API_PREFIX = "api/sys"; // 接口前缀
 const MSG_API_PREFIX = "api/msg"; // 接口前缀
